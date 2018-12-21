41f512283299e04529862d19dc51a2b5dfe12706
JOSE GALDINO <jose.galdino4@fatec.sp.gov.br>
2018-12-21 06:56:54 -0200

    Adicionar arquivos de projeto.

---
+++ src/ECOMMERCE.ApplicationCore/Entities/Cliente.cs
@@ -0,0 +1,13 @@
+﻿namespace ECOMMERCE.ApplicationCore.Entities
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+
+        }
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+    }
+}
